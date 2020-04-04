792d10b3ab2a8ec3b2b72be7827ceb6a1a362f6b
xtrp <36894883+xtrp@users.noreply.github.com>
2020-04-04 15:25:06 +0100

    add logging to routes

--- routes/api/market.js
+++ routes/api/market.js
@@ -3,46 +3,16 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const uuid = require('uuid');
+const fs = require('fs');
 
 const MarketItem = require('../../models/MarketItem');
-
-/*// @route   GET api/profile/me
-// @desc    Get current user's profile
-// @access  Private
-router.get('/me', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
-
-        if (!profile) {
-            return res.status(400).json({
-                errors: [
-                    {
-                        msg: 'No profile found for given user',
-                    },
-                ],
-            });
-        }
-
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});*/
 
 // @route   POST api/market
 // @desc    Create market item
 // @access  Private
 router.post(
     '/',
-    [
-        auth,
-        [
-            check('name', 'Name is required.').not().isEmpty(),
-            check('description', 'Description is required.').not().isEmpty(),
-            check('trader', 'Trader is required.').not().isEmpty(),
-        ],
-    ],
+    [auth, [check('name', 'Name is required.').not().isEmpty(), check('description', 'Description is required.').not().isEmpty()]],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -56,7 +26,7 @@ router.post(
             name: req.body.name,
             description: req.body.description,
             sold: false,
-            trader: req.body.trader,
+            trader: req.user.username,
             date: new Date(),
         };
 
@@ -66,6 +36,16 @@ router.post(
 
             await marketItem.save();
             res.json(marketItem);
+
+            // add to log
+            fs.appendFile(
+                './data/log.txt',
+                `"${marketItemFields.trader}" created a new market item titled "${marketItemFields.name}" at ${marketItemFields.date.toISOString()}`,
+                (err) => {
+                    if (err) return console.error(err.message);
+                    console.log(`Updated Log at ${new Date().toISOString()}`);
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
@@ -85,48 +65,6 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
-
-/*// @route   GET api/profile/user/:user_id
-// @desc    Get profile by user id
-// @access  Public
-router.get('/user/:user_id', async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
-
-        if (!profile) return res.status(400).json({ errors: [{ msg: 'Profile not found' }] });
-
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-
-        if (err.kind == 'ObjectId') {
-            return res.status(400).json({ errors: [{ msg: 'Profile not found' }] });
-        }
-
-        res.status(500).send('Server Error');
-    }
-});
-
-// @route   DELETE api/profile
-// @desc    Delete profile, user and posts
-// @access  Private
-router.delete('/', auth, async (req, res) => {
-    try {
-        // @todo --> remove user's posts
-
-        // remove profile
-        await Profile.findOneAndRemove({ user: req.user.id });
-
-        await User.findOneAndRemove({ _id: req.user.id });
-
-        res.json({
-            msg: 'User deleted',
-        });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});*/
 
 // @route   POST api/market/:item_id/offer
 // @desc    Add new offer to market item
@@ -192,6 +130,18 @@ router.post('/:item_id/offer', [auth, [check('content', 'Content is required.').
         await marketItem.save();
 
         res.json(marketItem);
+
+        // add to log
+        fs.appendFile(
+            './data/log.txt',
+            `"${newOffer.buyer}" made an offer for "${marketItem.name}" by trader "${marketItem.trader}" with the description "${
+                newOffer.description
+            }" at ${newOffer.date.toISOString()}`,
+            (err) => {
+                if (err) return console.error(err.message);
+                console.log(`Updated Log at ${new Date().toISOString()}`);
+            }
+        );
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -252,125 +202,22 @@ router.post('/:item_id/winningoffer', [auth, [check('offerId', 'Offer ID is requ
         await marketItem.save();
 
         res.json(marketItem);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
 
-/*// @route   DELETE api/profile/experience/:exp_id
-// @desc    Remove profile experience
-// @access  Private
-router.delete('/experience/:exp_id', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.user.id });
-
-        // Get index to remove
-        const removeIndex = profile.experience.map((item) => item.id).indexOf(req.params.exp_id);
-
-        profile.experience.splice(removeIndex, 1);
-
-        await profile.save();
-
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
-
-// @route   PUT api/profile/education
-// @desc    Add profile education
-// @access  Private
-router.put(
-    '/education',
-    [
-        auth,
-        [
-            check('school', 'Title is required').not().isEmpty(),
-            check('degree', 'Degree is required').not().isEmpty(),
-            check('fieldofstudy', 'Field of study is required').not().isEmpty(),
-            check('from', 'From date is required').not().isEmpty(),
-        ],
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { school, degree, fieldofstudy, from, to, current, description } = req.body;
-
-        const newEdu = {
-            school,
-            degree,
-            fieldofstudy,
-            from,
-            to,
-            current,
-            description,
-        };
-
-        try {
-            const profile = await Profile.findOne({ user: req.user.id });
-
-            profile.education.unshift(newEdu);
-
-            await profile.save();
-
-            res.json(profile);
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
-        }
-    }
-);
-
-// @route   DELETE api/profile/education/:exp_id
-// @desc    Remove profile education
-// @access  Private
-router.delete('/education/:edu_id', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.user.id });
-
-        // Get index to remove
-        const removeIndex = profile.education.map((item) => item.id).indexOf(req.params.edu_id);
-
-        profile.education.splice(removeIndex, 1);
-
-        await profile.save();
-
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
-
-// @route   GET api/profile/github/:username
-// @desc    Get user repos from Github
-// @access  Public
-router.get('/github/:username', (req, res) => {
-    try {
-        const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get(
-                'githubClientId'
-            )}&client_secret=${config.get('githubSecret')}`,
-            method: 'GET',
-            headers: { 'user-agent': 'node.js' },
-        };
-
-        request(options, (error, response, body) => {
-            if (error) console.error(error);
-            if (response.statusCode !== 200) {
-                return res.status(404).json({ errors: [{ msg: 'No GitHub Profile Found' }] });
+        // add to log
+        fs.appendFile(
+            './data/log.txt',
+            `"Trader "${marketItem.trader}" accepted the offer of "${winningOffer.description}" by buyer "${winningOffer.buyer}" for ${
+                marketItem.name
+            } at ${newOffer.date.toISOString()}`,
+            (err) => {
+                if (err) return console.error(err.message);
+                console.log(`Updated Log at ${new Date().toISOString()}`);
             }
-            res.json(JSON.parse(body));
-        });
+        );
     } catch (err) {
-        console.log(err.message);
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
-});*/
+});
 
 module.exports = router;
