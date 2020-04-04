b3eb975fd63b2310c3e9b5ca704e1794d21c4e22
xtrp <36894883+xtrp@users.noreply.github.com>
2020-04-04 18:42:41 +0100

    finish sign in page HTML, CSS, and JS

---
+++ client/assets/js/global.js
@@ -0,0 +1,44 @@
+const SERVER_BASE = 'http://localhost:7280';
+
+const verifyCredentials = async () => {
+    // get credentials from local storage
+    const credentials = JSON.parse(localStorage.getItem('minecraft-trading-market-credentials'));
+
+    // create authorization header
+    const authorizationHeader = 'Basic ' + btoa(credentials.username + ':' + credentials.pin);
+
+    const response = await fetch(SERVER_BASE + '/api/auth/', {
+        method: 'GET',
+        headers: new Headers({
+            Authorization: authorizationHeader,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }),
+    });
+
+    switch (response.status) {
+        case 200:
+            return 'accepted';
+            break;
+        case 401:
+            return 'denied';
+            break;
+        case 500:
+            return 'servererror';
+    }
+    return;
+};
+
+const setCredentials = (username, pin) => {
+    localStorage.setItem('minecraft-trading-market-credentials', JSON.stringify({ username, pin }));
+};
+
+// if credentials are valid, go to logged in homepage
+window.addEventListener('load', () => {
+    if (!window.location.href.toString().endsWith('home.html')) {
+        (async () => {
+            if ((await verifyCredentials()) == 'accepted') {
+                window.open('home.html', '_self');
+            }
+        })();
+    }
+});
